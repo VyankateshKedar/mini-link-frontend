60a64e656d71b01c1832ff79671a468b9337597f
Vyankatesh <125432163+VyankateshKedar@users.noreply.github.com>
2025-02-01 19:48:26 +0530

    Update ProtectedRoute.js

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,22 +2,13 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-/**
- * ProtectedRoute Component
- * @param {object} props - React props
- * @param {JSX.Element} props.children - The component to render if authenticated
- * @returns {JSX.Element} - Rendered component or redirect
- */
 const ProtectedRoute = ({ children }) => {
-  // Check if the user is authenticated
-   const isAuthenticated = !!sessionStorage.getItem("token"); // Adjust based on your auth logic
+  const isAuthenticated = !!sessionStorage.getItem("token");
 
   if (!isAuthenticated) {
-    // If not authenticated, redirect to AuthPage (login)
     return <Navigate to="/" replace />;
   }
 
-  // If authenticated, render the desired component
   return children;
 };
 
